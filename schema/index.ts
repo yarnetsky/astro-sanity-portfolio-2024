df3f21fb3f5d1f9d13f25a7505f7160fb28cf204
Jerry Yarnetsky <yarnete@miamioh.edu>
2024-08-28 12:23:27 -0400

    add page to schemas

--- schema/index.ts
+++ schema/index.ts
@@ -1,4 +1,5 @@
 import blockContent from './blockContent'
 import post from './post'
+import page from './page';
 
-export const schemaTypes = [post, blockContent]
+export const schemaTypes = [post, page, blockContent]
